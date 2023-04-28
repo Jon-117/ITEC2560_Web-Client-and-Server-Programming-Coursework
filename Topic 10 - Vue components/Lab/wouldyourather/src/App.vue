fdbb5506978add68b5619f0f9c30bf795d0ce5a3
jkono <j@je>
2023-04-28 10:10:08 -0500

    Topic 10 - Lab finished

--- Topic 10 - Vue components/Lab/wouldyourather/src/App.vue
+++ Topic 10 - Vue components/Lab/wouldyourather/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
 
-      <h1>Would You Rather...</h1>
+      <h1>I Would Rather...</h1>
 
-      <would-you-rather v-bind:question1="question1"
-                        v-bind:answer1="answer1"
-                        v-bind:answer2="answer2"
-                        v-on:answer-changed="answerChanged"
+      <would-you-rather
+              v-for="question in questions"
+              v-bind:question="question"
+              v-on:answer-changed="answerChanged"
       >
       </would-you-rather>
 
-      <p>{{userSelectionMessage}}</p>
+<!--      <p>{{ userSelectionMessage }}</p>-->
+
+      <div class="answers" v-if="questions[0].userAnswer !== '' && questions[1].userAnswer !== ''  && questions[2].userAnswer !== '' ">
+          <h2>I would rather...</h2>
+          <p v-for="answer in answers"> {{ answer }} </p>
+      </div>
 
   </div>
 </template>
 
 <script>
-import WouldYouRather from './components/WouldYouRather.vue'
+import WouldYouRather from './components/WouldYouRatherQuestion.vue'
 
 export default {
         name: 'App',
@@ -25,17 +30,47 @@ export default {
         },
         data(){
                 return {
-                        question1: 'lick peanut butter off a hobo\'s foot or wrestle a tiger?',
-                        answer1: 'Lick peanut butter off a hobo\'s foot',
-                        answer2: 'Wrestle a tiger',
-                        userSelectionMessage: '',
+                        questions: [
+                                {
+                                        id: 1,
+                                        question: 'Lick peanut butter off a hobo\'s foot or wrestle a tiger?',
+                                        answer1: 'Lick peanut butter off a hobo\'s foot',
+                                        answer2: 'Wrestle a tiger',
+                                        userAnswer: ''
+                                },
+                                {
+                                        id: 2,
+                                        question: 'Go back to age 5 with everything you know now or know now everything your future self will learn?',
+                                        answer1: 'Go back to age 5',
+                                        answer2: 'Know everything my future self will learn',
+                                        userAnswer: ''
+
+                                },
+                                {
+                                        id: 3,
+                                        question: 'Travel the world for a year on a shoestring budget or stay in only one country for a year but live in luxury?',
+                                        answer1: 'Travel the world',
+                                        answer2: 'Live in luxury',
+                                        userAnswer: ''
+
+                                }],
+                        answers: [],
+
+                        userSelectionMessage: 'I would rather... ',
                 }
         },
         methods: {
-                answerChanged(choice) {
-                        this.userSelectionMessage = `Thanks for your answer! You chose ${choice}`
+                answerChanged(question, choice) {
+                        this.questions.find(function (s){
+                                if (s.id === question.id) {
+                                        s.userAnswer = choice
+                                }
+                        })
+                        this.answers.push(choice.toLowerCase())
                 }
-        }
+        },
+
+
 }
 
 </script>
@@ -46,7 +81,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+    background-color: black;
+  color: #ffffff;
   margin-top: 60px;
 }
+
 </style>
---
+++ Topic 10 - Vue components/Lab/wouldyourather/src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+    <div class="question" >
+      <h3>{{ question.question }}</h3>
+      <label><input type="radio" v-model="choice" v-bind:value="question.answer1" v-on:change="choiceMade">
+      {{ question.answer1 }}</label>
+
+      <label><input type="radio" v-model="choice" v-bind:value="question.answer2" v-on:change="choiceMade">
+      {{ question.answer2 }}</label>
+
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+        name: 'WouldYouRather',
+        props: {
+                question: Object
+        },
+        data(){
+                return{
+                        choice:''
+                }        },
+        methods: {
+                choiceMade() {
+                        this.$emit("answer-changed", this.question, this.choice)
+                }
+        }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.question {
+    background-color: goldenrod;
+}
+</style>
